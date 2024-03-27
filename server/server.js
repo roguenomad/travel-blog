d6ebb2a5483446d55e365baebcf62c61e2349ce0
Nohely Garcia <135940276+roguenomad@users.noreply.github.com>
2024-03-27 15:29:58 -0700

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+
+
+// app.set('view engine', 'ejs')
+
+// app.get('/', (req, res) => {
+//     console.log('Here')
+//     res.render('index')
+// })
+
+
+//Error message
+// app.get('/', (req, res) => {
+//     console.log('Here')
+//     res.status(500).send("Error Code 500")
+// })
+
+app.listen(8000)
