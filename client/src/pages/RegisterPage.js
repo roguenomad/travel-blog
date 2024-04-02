2be626aa44829ea0d2d32a5aa0ea01523f7cfd9f
Nohely Garcia <135940276+roguenomad@users.noreply.github.com>
2024-04-02 08:00:28 -0700

    login/register

---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,11 @@
+export default function RegisterPage() {
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder="username"/>
+                <input type="password" placeholder="password"/>
+                <button>Register</button>
+            </form>
+        </div>
+    );
+}
