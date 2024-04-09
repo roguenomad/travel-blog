60411d5bb3bdcca17584b8ae9955749e4e82cefe
Nohely Garcia <135940276+roguenomad@users.noreply.github.com>
2024-04-09 09:13:53 -0700

    hero-image-update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,18 @@
 import '../App.css';
-import heroImg from '../images/rice-fields-ind.jpeg'
+// import heroImg from '../images/rice-fields-ind.jpeg'
 
 const Home = () => {
     return (
         <div className="hero">
-            <div className="hero-bg"></div>
-            <div>
-                <img className='hero-img' src={ heroImg } />
+            <div className='hero-text'>
+                <h3><span>Welcome to Rogue Nomad! A travel website dedicated to those who </span><br/> 
+                <span>love to travel by immersing themselves into the culture, people, and </span><br/> 
+                <span>places they’re visiting.</span></h3>
+                <h3><span>We are a human and a cat, on a quest to connect with other humans.</span></h3>
+                <h3><span>Please follow along on our journey, and learn how to start yours.</span></h3>
             </div>
+            <div className='hero-img'></div>
+            
         </div>
     )
 };
