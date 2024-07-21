74081ce6654a0f48b4c98a237e2d2ab88d2f5e71
Nohely Garcia <135940276+roguenomad@users.noreply.github.com>
2024-07-21 11:43:41 -0700

    mob-opt-upd

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,16 +8,16 @@ const Home = () => {
                 <div className="overlay">
                 <div className='hero-text'>
                     <h2>Welcome to <span className='rn-span'>Rogue Nomad!</span></h2>
-                    <h4>A website <b> dedicated </b><br /> to those who love to
-                        <b> travel</b> by immersing themselves into the 
-                        <b> culture, people, and places</b> they’re visiting. 
+                    <h4>A website <b> dedicated </b>to <br></br>those who love to
+                        <b> travel by<br></br></b> immersing themselves into the 
+                        <b><br></br> culture, people, and places</b><br></br>they’re visiting. 
                         </h4>
-                    <h3><span>Follow along on our journey, and learn how to start yours.</span></h3>
+                    {/* <h3><span>Follow along on our journey, and learn how to start yours.</span></h3> */}
                     <div className='hero-btn-bdr'><button className='hero-button'>Start Here</button></div>
                 </div>
                 </div>
             </div>
-            <Postcards />
+            {/* <Postcards /> */}
         </>
     )
 };
--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -1,38 +1,31 @@
 import React from 'react'
-import { Outlet, NavLink, Link } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import { Navbar } from "../components/Navbar"
 import logo from '../assets/rn-logo.png'
 import insta from '../assets/insta-icon.png'
+import pint from '../assets/pin-icon.png'
+import email from '../assets/email-icon.png'
+import { NavDropdown } from 'react-bootstrap';
 
 const Layout = () => {
     return (
-           <div className='navBar'> <img src={logo} alt='Logo' className='logo-img'/>;
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to = "/" className="nav-link"> Home </NavLink>
-                    </li>
-                    <li activeClassName="selected"> 
-                        <NavLink to = "/destinations" className="nav-link"> Destinations </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to = "/plan" className="nav-link"> Trip Planner </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to = "/blogs" className="nav-link"> Extras </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to = "/about" className="nav-link"> About </NavLink>
-                    </li>
-                </ul>
-            </nav>
-            {/* <div className='social-icons'>
-            <Link to="https://www.instagram.com/connected_confessions/" target="_blank"><img src={insta} className="githubIcon" /></Link>
-            <Link to="https://www.instagram.com/connected_confessions/" target="_blank"><img src={insta} className="githubIcon" /></Link>
-            <Link to="https://www.instagram.com/connected_confessions/" target="_blank"><img src={insta} className="githubIcon" /></Link>                                
-            </div> */}
-            <Outlet />
+        <div className='layoutBody'> 
+            <Link to="/"><img src={logo} alt='Logo' className='logo-img'/></Link>;
+            <div className='social-icons'>
+            <Link to="https://www.instagram.com/connected_confessions/" target="_blank"><img src={insta} className="socialIcon githubIcon" alt='instagram' /></Link>
+            <Link to="https://www.pinterest.com/connectedconfessions/" target="_blank"><img src={pint} className="socialIcon githubIcon" alt='pinterest' /></Link>
+            <Link to="mailto:connectedconfessions@gmail.com" target="_blank"><img src={email} className="socialIcon githubIcon" alt='email' /></Link>                                
+            </div>
+            <div className='navBar'>
+                <Navbar />
+            </div>
+            <div>
+                <Outlet />
+            </div>
         </div>
     )
 };
 
+
 export default Layout;
+
